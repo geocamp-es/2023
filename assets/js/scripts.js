14d26c5361272b82346a667eb825ed49ad1f4c31
michogar <michogar@gmail.com>
2023-09-06 19:29:41 +0200

    Arregla menu navegacion

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function() {
 		else if(element_class == 'menu-project') { scroll_to = $(".about").offset().top - nav_height - 60; }
 		// else if(element_class == 'menu-testimonials') { scroll_to = $(".testimonials").offset().top - nav_height - 60; }
 		else if(element_class == 'menu-about-us') { scroll_to = $(".whos-behind").offset().top - nav_height - 60; }
-		else if(element_class == 'menu-camiseta') { scroll_to = $(".camiseta").offset().top - nav_height - 60; }
+		else if(element_class == 'menu-camiseta') { scroll_to = $(".faq").offset().top - nav_height - 60; }
 		else if(element_class == 'menu-patrocinadores') { scroll_to = $(".patrocinadores").offset().top - nav_height - 60; }
 		else if(element_class == 'menu-mapa') { scroll_to = $(".map").offset().top - nav_height /*- 60*/; }
 		else if(element_class == 'menu-organizacion') { scroll_to = $(".organizadores").offset().top - nav_height - 60; }
